86b86b6dea4373e0ad399026b7ff285b4f365f86
Ethan Ellerstein <eellerstein@gmail.com>
2023-11-07 23:30:21 -0600

    updated index.html

---
+++ app.js
@@ -0,0 +1,21 @@
+// app.js
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error loading the file');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000/');
+});
