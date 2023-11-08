008c58fcda68f8e5f9b8ab3151a3f8df4239ae46
Ethan Ellerstein <eellerstein@gmail.com>
2023-11-08 00:06:32 -0600

    updated dialog_flow and speech_recognition

---
+++ dialog_flow.js
@@ -0,0 +1,25 @@
+// dialogflow.js
+const dialogflow = require('dialogflow');
+const fetch = require('node-fetch');
+
+const sessionClient = new dialogflow.SessionsClient();
+const projectId = 'Y105644911645370616526'; // Replace with your Dialogflow project ID
+
+async function detectIntent(query, sessionId) {
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  const responses = await sessionClient.detectIntent(request);
+  const result = responses[0].queryResult;
+  return result;
+}
+
+module.exports = detectIntent;
--- speech_recognition.js
+++ speech_recognition.js
@@ -1,18 +1,25 @@
 // speech-recognition.js
 const annyang = require('annyang');
 const fetch = require('node-fetch');
+const detectIntent = require('./dialogflow'); // Your Dialogflow setup code from dialogflow.js
 
 // Initialize annyang
 annyang.init();
 
-// Add voice commands and their corresponding functions
-annyang.addCommands({
-  'hello': () => {
-    console.log('Hello!');
-  },
-  'what is the time': () => {
-    // Implement time-related logic here
-  },
+// Event listener for speech recognition
+annyang.addCallback('result', (userSaid) => {
+  // Use Dialogflow to detect intent and handle userSaid
+  const sessionId = 'unique-session-id'; // You can use a unique identifier for each session
+  detectIntent(userSaid, sessionId)
+    .then((dialogflowResponse) => {
+      const fulfillmentText = dialogflowResponse.fulfillmentText;
+      console.log('Assistant says:', fulfillmentText);
+      // Respond to the user with fulfillmentText
+    })
+    .catch((error) => {
+      console.error('Dialogflow error:', error);
+      // Handle the error
+    });
 });
 
 // Start listening for voice commands
